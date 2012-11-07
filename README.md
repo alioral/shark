9094c09ca8981b3b65c298e85c5ba6d80c9cb9e8
Ali Oral <alioral@Ali-MacBook-Pro.local>
2012-11-07 03:51:10 +0200

    Shark - Your helper for finding talent

---
+++ README.md
@@ -0,0 +1,41 @@
+<h2>ShARK</h2>
+
+<p>Shark is a web crawler that helps you *possibly* 
+find great coders using the Github Network. </p>
+
+<p><b>Idea:</b> Since nowadays every tech company from startups to huge corporations, 
+take Github into consideration and would like to hire best hackers, 
+ShARK helps them out by relying on some of the facts I've pre-defined.
+It all starts by the user choosing a company. 
+The reason is for that we would *again, possibly* 
+trust the endorsements(in this case endorsements are being followed by the company member) 
+of engineers from companies such as Facebook, Twitter etc. 
+So the crawler basic brings you the Github profiles that are 
+possibly great hackers by taking connections in consideration.
+(Note from the Author: Of course, # of repos, starts were some of the
+conditions I could take into consideration, but hey, what is better
+than being followed by a talented engineer?)
+</p>
+
+<p>
+	<h3>Parameters:</h3>
+		<code>shark.py <github_company_url> <depth> <num_of_hackers></code>
+	</p>
+<p>
+	<h3>Example: </h3>
+	<code>python shark.py https://github.com/joyent 2</code>
+</p>
+<p>
+	<b>Algorithm:</b>
+	
+	<ol>
+		<li>For given company URL, retrieve all the members</li>
+		<li>For each member, find the people who are followed by that 
+		member and insert them into a hashtable with rank.</li>
+		<li>If a particular person shows up again increment its rank.</li>
+		<li>When all the members' followings scanned, sort the hashtable according to rank, 
+		bring the top N developers from the hashtable.</li>
+	</ol>
+</p>
+<p>If the depth is defined, the crawler will look for the friends of friends of the member and etc. 
+(Depth is defined, in order to prevent the infamous "Six Degrees of Kevin Bacon"</p>
---
+++ shark.py
@@ -0,0 +1,110 @@
+import sys
+import urllib2
+from collections import defaultdict
+from bs4 import BeautifulSoup
+
+hackerTable = defaultdict(int)
+
+def getMembers(url):
+  text = urllib2.urlopen(url).read();
+  soup = BeautifulSoup(text);
+  memberList = []
+
+  data = soup.findAll('ul',attrs={'class':'members-list'});
+  for div in data:
+    links = div.findAll('li')
+    for link in links:
+		  memberList.append("https://github.com" + str(link.a['href']))
+	
+  return memberList
+
+def getFollowings(url):
+	text = urllib2.urlopen(url).read();
+	soup = BeautifulSoup(text);
+
+	friendList = []
+	data = soup.findAll('ul',attrs={'class':'members'});
+	if len(data) > 0:
+		for div in data:
+			links = div.findAll('li')
+  		if len(links) > 0:
+  			for link in links:
+  				friendList.append(link.a['href'])
+  				hackerTable[link.a['href']] += 1
+  				print 'Link that goes into hashtable: ' + link.a['href']
+		  	 
+				print 'Member: ' + str(link.a['href'])
+  
+  
+	return friendList
+
+def getAllFollowings(url):
+	i,totalFollowers = 1, []
+	followerList = getFollowings(url + '/following?page=' + str(i))
+	
+	while(len(followerList) != 0):
+		print "followerList: " + str(len(followerList))
+		i += 1
+		followerList = getFollowings(url + '/following?page=' + str(i))
+	
+	return
+
+'''
+employeeList = getMembers("https://github.com/vmware?tab=members")
+    
+topHackerCount = 2 #Select the top N hacker to crawl
+'''
+
+def helloWorld():
+	print 'Hello, World'
+
+def crawlWithDepth(depth, hackersToCrawl, topHackerCount):
+	if depth > 0:
+		for i in xrange(len(hackersToCrawl)):
+			#given List of People Retrieve all of their followings.
+			#Insert them to hash and increase their rank by 1 in each occurence
+			print 'Hacker Name: ' + hackersToCrawl[i]
+			getAllFollowings(hackersToCrawl[i])
+		
+		topHackers, counter = [], 0
+			
+			#sort the hacker hash and choose N best hackers among the others
+		sortedHackerHash = sorted(hackerTable, key=hackerTable.get, reverse=False)
+			
+		for hacker in sortedHackerHash:
+			if counter >= topHackerCount:
+				break
+			topHackers.append('https://www.github.com' + hacker)
+			counter += 1
+		
+		print 'My Top Hackers: ' + str(topHackers)
+		
+		if len(topHackers) < 1: # If you end up getting no hackers at all
+			return hackersToCrawl #return the previous full list
+		
+		hackerTable.clear() #clear the hash for the new hackers
+		return crawlWithDepth(depth - 1, topHackers, topHackerCount)
+	else:
+		return str(hackersToCrawl)
+		
+#default settings
+
+url = "https://github.com/vmware?tab=members"; 
+topHackerCount = 2
+depth = 3
+
+#end of default settings
+
+if (len(sys.argv) > 1):
+	url = sys.argv[1] + '?tab=members'
+
+if (len(sys.argv) > 2):
+	depth = int(sys.argv[2])
+
+if (len(sys.argv) > 3):
+	topHackerCount = sys.argv[3]
+
+
+employeeList = getMembers(url)
+hackers =  crawlWithDepth(depth, employeeList, topHackerCount)
+print 'Guys you should take a look: ' + str(hackers)
